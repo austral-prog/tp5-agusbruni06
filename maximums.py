c2fa23ac928db63d736b5cf27671d1d3f4bfa2a8
agusbruni06 <agus.bruni2019@gmail.com>
2025-12-04 15:39:36 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -2,9 +2,12 @@
 
 def max_of_two(x, y):
     """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
+    return max(x, y)
 
+print(max_of_two(3, 5))
 
 def max_of_three(x, y, z):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    return max(x, y, z)
+
+print(max_of_three(3, 5, 2))
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,44 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    discriminante = b**2 - 4*a*c
+    if discriminante > 0:
+        r1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+        r2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+        result = f"({r1}, {r2})"
+        return result
+    elif discriminante == 0:
+        r1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+        r2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+        result = f"({r1})"
+        return result
+    else:
+        result = "( )"
+        return result
+   
+    #result = f"({r1}, {r2})"
+    #return result
+
+A = int(input("ingrese parametro a: "))
+B = int(input("ingrese parametro b: "))
+C = int(input("ingrese parametro c: "))
+print(roots(A, B, C))
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    valory = a*x**2 + b*x + c
+    return valory
 
+ejex = int(input("ingrese valor de x: "))
+print(value_y(A, B, C, ejex))
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    ecuacion1 = f"f(x) = {a}X^2 + {b}X + {c}"
+    return ecuacion1
+print(to_string(A, B, C))
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    ecuacion2 = f"f'(x) = {2*a}x + {b} + {c*0}"
+    return ecuacion2
+print(derivation(A, B, C))
